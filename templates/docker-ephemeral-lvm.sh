9a199c738d6f01c7dd5d7619e44324e59bc4a6b8
gmanipon <gmanipon@jpl.nasa.gov>
2018-01-23 16:34:23 +0000

    update generalized docker-ephemeral-lvm.sh and latest harikiri.py

--- templates/docker-ephemeral-lvm.sh
+++ templates/docker-ephemeral-lvm.sh
@@ -1,29 +1,96 @@
 #!/bin/sh -e
-# This script will DESTROY /dev/xvdb|nvme0n1 and remount it for HySDS work dir.
-# This script will DESTROY /dev/xvdc|nvme1n1 and remount it for Docker volume storage.
-# It is intended for EC2 instances with 2 ephemeral SSD instance stores like 
-# the c3.xlarge and i3.4xlarge instance types.
+# This script will DESTROY the first ephemeral/EBS volume and remount it for HySDS work dir.
+# This script will DESTROY the second ephemeral/EBS volume and remount it for Docker volume storage.
 
-systemctl stop docker || true
+# update system first
+yum update -y || true
 
-# Setup Instance Store 1 for Docker volume storage
-if [[ -e "/dev/nvme1n1" ]]; then
-  DEV="/dev/nvme1n1"
+# get user/group
+if [[ -d "/home/ops" ]]; then
+  user="ops"
+  group="ops"
+elif [[ -d "/home/ec2-user" ]]; then
+  user="ec2-user"
+  group="ec2-user"
 else
-  DEV="/dev/xvdc"
+  user="ops"
+  group="ops"
 fi
-if [[ -e "$DEV" ]]; then
+
+# get docker daemon start/stop commands
+if [[ -e "/bin/systemctl" ]]; then
+  start_docker="systemctl start docker"
+  stop_docker="systemctl stop docker"
+else
+  start_docker="service docker start"
+  stop_docker="service docker stop"
+fi
+
+$stop_docker
+
+# get ephemeral storage devices
+EPH_BLK_DEVS=( `curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/ | grep ^ephemeral | sort` )
+EPH_BLK_DEVS_CNT=${#EPH_BLK_DEVS[@]}
+echo "Number of ephemeral storage devices: $EPH_BLK_DEVS_CNT"
+
+# get EBS block devices
+EBS_BLK_DEVS=( `curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/ | grep ^ebs | sort` )
+EBS_BLK_DEVS_CNT=${#EBS_BLK_DEVS[@]}
+echo "Number of EBS block devices: $EBS_BLK_DEVS_CNT"
+
+# delegate devices for HySDS work dir and docker storage volumes
+if [ "$EPH_BLK_DEVS_CNT" -ge 2 ]; then
+  DATA_DEV=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EPH_BLK_DEVS[0]})
+  DOCKER_DEV=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EPH_BLK_DEVS[1]})
+elif [ "$EPH_BLK_DEVS_CNT" -eq 1 ]; then
+  DATA_DEV=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EPH_BLK_DEVS[0]})
+  if [ "$EBS_BLK_DEVS_CNT" -ge 1 ]; then
+    DOCKER_DEV=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EBS_BLK_DEVS[0]})
+  else
+    DOCKER_DEV=/dev/xvdc
+  fi
+else
+  if [ "$EBS_BLK_DEVS_CNT" -ge 2 ]; then
+    DATA_DEV=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EBS_BLK_DEVS[0]})
+    DOCKER_DEV=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EBS_BLK_DEVS[1]})
+  elif [ "$EBS_BLK_DEVS_CNT" -eq 1 ]; then
+    DATA_DEV=/dev/$(curl -s http://169.254.169.254/latest/meta-data/block-device-mapping/${EBS_BLK_DEVS[0]})
+    DOCKER_DEV=/dev/xvdc
+  else
+    DATA_DEV=/dev/xvdb
+    DOCKER_DEV=/dev/xvdc
+  fi
+fi
+
+# resolve symlinks
+DATA_DEV=$(readlink -f $DATA_DEV)
+DOCKER_DEV=$(readlink -f $DOCKER_DEV)
+
+# i3 instances utilizing NVMe will have incorrect mount in EC2 metadata; handle this case
+if [[ ! -e "$DATA_DEV" && -e "/dev/nvme0n1" ]]; then
+  DATA_DEV=/dev/nvme0n1
+fi
+if [[ ! -e "$DOCKER_DEV" && -e "/dev/nvme1n1" ]]; then
+  DOCKER_DEV=/dev/nvme1n1
+fi
+
+# log devices
+echo "DATA_DEV: $DATA_DEV"
+echo "DOCKER_DEV: $DOCKER_DEV"
+
+# Setup docker volume storage
+if [[ -e "$DOCKER_DEV" ]]; then
   # clean out docker
   rm -rf /var/lib/docker
 
   # unmount block device if not already
-  umount $DEV 2>/dev/null || true
+  umount $DOCKER_DEV 2>/dev/null || true
 
   # remove volume group
   vgremove -ff vg-docker || true
 
   # remove physical volume
-  pvremove -ff $DEV || true
+  pvremove -ff $DOCKER_DEV || true
 
   # install cryptsetup
   yum install -y cryptsetup || true
@@ -32,20 +99,20 @@ if [[ -e "$DEV" ]]; then
   PASSPHRASE=`hexdump -n 16 -e '4/4 "%08X" 1 "\n"' /dev/random`
 
   # format the ephemeral volume with selected cipher
-  echo $PASSPHRASE | cryptsetup luksFormat -c twofish-xts-plain64 -s 512 --key-file=- $DEV
+  echo $PASSPHRASE | cryptsetup luksFormat -c twofish-xts-plain64 -s 512 --key-file=- $DOCKER_DEV
 
   # open the encrypted volume to a mapped device
-  echo $PASSPHRASE | cryptsetup luksOpen --key-file=- $DEV ephemeral-encrypted
+  echo $PASSPHRASE | cryptsetup luksOpen --key-file=- $DOCKER_DEV ephemeral-encrypted
 
   # set name of mapped device
-  DEV_ENC="/dev/mapper/ephemeral-encrypted"
+  DOCKER_DEV_ENC="/dev/mapper/ephemeral-encrypted"
 
   # determine 75% of volume size to be used for docker data
-  DATA_SIZE=`lsblk -b $DEV | grep disk | awk '{printf "%.0f\n", $4/1024^3*.75}'`
+  DATA_SIZE=`lsblk -b $DOCKER_DEV | grep disk | awk '{printf "%.0f\n", $4/1024^3*.75}'`
 
   # create physical volume and volume group for docker
-  pvcreate -ff $DEV_ENC
-  vgcreate -ff  vg-docker $DEV_ENC
+  pvcreate -ff $DOCKER_DEV_ENC
+  vgcreate -ff  vg-docker $DOCKER_DEV_ENC
 
   # reconfigure docker storage for devicemapper
   echo "STORAGE_DRIVER=devicemapper" > /etc/sysconfig/docker-storage-setup
@@ -58,23 +125,16 @@ if [[ -e "$DEV" ]]; then
   sed -i 's# "# --storage-opt dm.basesize=100GB "#' /etc/sysconfig/docker-storage
 fi
 
-systemctl start docker
-
-# Setup Instance Store 0 for HySDS work dir (/data) if mounted as /mnt
+# Setup HySDS work dir (/data) if mounted as /mnt
 DATA_DIR="/data"
-if [[ -e "/dev/nvme0n1" ]]; then
-  DATA_DEV="/dev/nvme0n1"
-else
-  DATA_DEV="/dev/xvdb"
-fi
 if [[ -e "$DATA_DEV" ]]; then
-  # clean out /mnt, /data and /data.orig
+  # clean out /mnt, ${DATA_DIR} and ${DATA_DIR}.orig
   rm -rf /mnt/cache /mnt/jobs /mnt/tasks
-  rm -rf /data/work/cache /data/work/jobs /data/work/tasks
-  rm -rf /data.orig
+  rm -rf ${DATA_DIR}/work/cache ${DATA_DIR}/work/jobs ${DATA_DIR}/work/tasks
+  rm -rf ${DATA_DIR}.orig
 
-  # backup /data/work and index style
-  cp -rp /data /data.orig || true
+  # backup ${DATA_DIR}/work and index style
+  cp -rp ${DATA_DIR} ${DATA_DIR}.orig || true
 
   # unmount block device if not already
   umount $DATA_DEV 2>/dev/null || true
@@ -97,12 +157,15 @@ if [[ -e "$DATA_DEV" ]]; then
   # format XFS
   mkfs.xfs -f $DATA_DEV_ENC
 
-  # mount as /data
+  # mount as ${DATA_DIR}
+  mkdir -p $DATA_DIR || true
   mount $DATA_DEV_ENC $DATA_DIR
 
-  # set permissions
-  chown -R ops:ops /data || true
-
   # copy work and index style
-  cp -rp /data.orig/work /data/ || true
+  cp -rp ${DATA_DIR}.pristine/work ${DATA_DIR}/ || true
+
+  # set permissions
+  chown -R ${user}:${group} ${DATA_DIR} || true
 fi
+
+$start_docker
--- templates/harikiri.py
+++ templates/harikiri.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
-import os, sys, time, re, requests, logging, argparse, traceback, backoff
+"""
+HySDS inactivity daemon to perform scale down of auto scaling group/spot fleet
+request and perform self-termination (harikiri) of the instance. If a keep-alive 
+signal file exists at <root_work_dir>/.harikiri, then self-termination is bypassed
+until it is removed.
+"""
+import os, sys, time, re, json, socket, requests, logging, argparse, traceback, backoff
 from random import randint
 from subprocess import call
 from datetime import datetime
+from pprint import pformat
 import boto3
 from botocore.exceptions import ClientError
 
@@ -15,13 +22,59 @@ DAY_DIR_RE = re.compile(r'jobs/\d{4}/\d{2}/\d{2}/\d{2}/\d{2}$')
 
 NO_JOBS_TIMER = None
 
+KEEP_ALIVE = False
 
-def is_jobless(root_work, inactivity_secs):
+
+def log_event(url, event_type, event_status, event, tags):
+    """Log custom event."""
+
+    params = {
+        'type': event_type,
+        'status': event_status,
+        'event': event,
+        'tags': tags,
+        'hostname': socket.getfqdn(),
+    }
+    headers = { 'Content-type': 'application/json' }
+    r = requests.post("%s/event/add" % url, data=json.dumps(params), 
+                      verify=False, headers=headers)
+    r.raise_for_status()
+    resp = r.json()
+    return resp
+
+    
+def keep_alive(root_work):
+    """Check if the keep alive signal exists."""
+
+    return True if os.path.exists(os.path.join(root_work, ".harikiri")) else False
+
+
+def is_jobless(root_work, inactivity_secs, logger=None):
     """Check if no jobs are running and hasn't run in the past 
        amount of time passed in.
     """
 
     global NO_JOBS_TIMER
+    global KEEP_ALIVE
+
+    # check if keep-alive
+    logging.info("KEEP_ALIVE: %s" % KEEP_ALIVE)
+    if keep_alive(root_work):
+        if KEEP_ALIVE != True:
+            KEEP_ALIVE = True
+            if logger is not None:
+                try: print(log_event(logger, 'harikiri', 'keep_alive_set', {}, []))
+                except: pass
+        logging.info("Keep-alive exists.")
+        return
+    else:
+        if KEEP_ALIVE != False:
+            KEEP_ALIVE = False
+            if logger is not None:
+                try: print(log_event(logger, 'harikiri', 'keep_alive_unset', {}, []))
+                except: pass
+            logging.info("Keep-alive removed.")
+
     most_recent = None
     for root, dirs, files in os.walk(root_work, followlinks=True):
         match = DAY_DIR_RE.search(root)
@@ -53,6 +106,41 @@ def get_all_groups(c):
     return c.describe_auto_scaling_groups()['AutoScalingGroups']
 
 
+@backoff.on_exception(backoff.expo, ClientError, max_tries=10, max_value=512)
+def get_all_fleets(c):
+    """Get all Spot Fleet requests."""
+
+    fleets = []
+    next_token = None
+    while True:
+        if next_token is None:
+            resp = c.describe_spot_fleet_requests()
+        else:
+            resp = c.describe_spot_fleet_requests(NextToken=next_token)
+        fleets.extend(resp['SpotFleetRequestConfigs'])
+        next_token = resp.get('NextToken', None)
+        if next_token is None: break
+    return fleets
+
+
+@backoff.on_exception(backoff.expo, ClientError, max_tries=10, max_value=512)
+def get_fleet_instances(c, fleet_name):
+    """Get all Spot Fleet instances for a Spot Fleet request."""
+
+    instances = []
+    next_token = None
+    while True:
+        if next_token is None:
+            resp = c.describe_spot_fleet_instances(SpotFleetRequestId=fleet_name)
+        else:
+            resp = c.describe_spot_fleet_instances(SpotFleetRequestId=fleet_name,
+                                                   NextToken=next_token)
+        instances.extend(resp['ActiveInstances'])
+        next_token = resp.get('NextToken', None)
+        if next_token is None: break
+    return instances
+
+
 @backoff.on_exception(backoff.expo, ClientError, max_value=512)
 def detach_instance(c, as_group, id):
     """Detach instance from AutoScaling group."""
@@ -60,7 +148,24 @@ def detach_instance(c, as_group, id):
                        ShouldDecrementDesiredCapacity=True)
 
 
-def seppuku():
+@backoff.on_exception(backoff.expo, ClientError, max_value=512)
+def decrement_fleet(c, spot_fleet):
+    """Decrement target capacity of spot fleet."""
+
+    resp = c.describe_spot_fleet_requests(SpotFleetRequestIds=[spot_fleet])
+    tg = resp['SpotFleetRequestConfigs'][0]['SpotFleetRequestConfig']['TargetCapacity']
+    logging.info("TargetCapacity: %s" % tg)
+    tg -= 1
+    if tg > 0:
+        c.modify_spot_fleet_request(ExcessCapacityTerminationPolicy='NoTermination',
+                                    SpotFleetRequestId=spot_fleet, TargetCapacity=tg)
+    else:
+        c.cancel_spot_fleet_requests(SpotFleetRequestIds=[spot_fleet],
+                                     TerminateInstances=False)
+    logging.info("response: %s" % pformat(resp))
+
+
+def seppuku(logger=None):
     """Shutdown supervisord and the instance if it detects that it is 
        currently part of an autoscale group."""
 
@@ -71,8 +176,11 @@ def seppuku():
     logging.info("Meditating for %s seconds to avoid thundering herd." % meditation_time)
     time.sleep(meditation_time)
 
-    # check if instance part of an autoscale group
+    # instances may be part of autoscaling group or spot fleet
     as_group = None
+    spot_fleet = None
+
+    # check if instance part of an autoscale group
     id = str(requests.get('http://169.254.169.254/latest/meta-data/instance-id').content)
     logging.info("Our instance id: %s" % id)
     c = boto3.client('autoscaling')
@@ -87,24 +195,36 @@ def seppuku():
                 logging.info("Matched!")
                 break
     if as_group is None:
-        logging.info("This instance %s is not part of any autoscale group. Cancelling seppuku." % id)
-        return
+        logging.info("This instance %s is not part of any autoscale group." % id)
+
+        # check if instance is part of a spot fleet
+        c = boto3.client('ec2')
+        for fleet in get_all_fleets(c):
+            fleet_name = str(fleet['SpotFleetRequestId'])
+            logging.info("Checking fleet: %s" % fleet_name)
+            for i in get_fleet_instances(c, fleet_name):
+                sf_inst_id = str(i['InstanceId'])
+                logging.info("Checking fleet instance: %s" % sf_inst_id)
+                if id == sf_inst_id:
+                    spot_fleet = fleet_name
+                    logging.info("Matched!")
+                    break
+        if spot_fleet is None:
+            logging.info("This instance %s is not part of any spot fleet." % id)
+            return
 
     # gracefully shutdown
     while True:
-        try: graceful_shutdown(as_group, id)
+        try: graceful_shutdown(as_group, spot_fleet, id, logger)
         except Exception, e:
             logging.error("Got exception in graceful_shutdown(): %s\n%s" %
                           (str(e), traceback.format_exc()))
         time.sleep(randint(0, 600))
 
 
-def graceful_shutdown(as_group, id):
-    """Gracefully shutdown supervisord, detach from AutoScale group,
+def graceful_shutdown(as_group, spot_fleet, id, logger=None):
+    """Gracefully shutdown supervisord, detach from AutoScale group or spot fleet,
        and shutdown."""
-
-    # get client
-    c = boto3.client('autoscaling')
 
     # stop docker containers
     try:
@@ -123,12 +243,25 @@ def graceful_shutdown(as_group, id):
 
     # detach and die
     logging.info("Committing seppuku.")
-    detach_instance(c, as_group, id)
+
+    if as_group is not None:
+        c = boto3.client('autoscaling')
+        detach_instance(c, as_group, id)
+    else:
+        c = boto3.client('ec2')
+        decrement_fleet(c, spot_fleet)
+
     time.sleep(60)
+
+    # log seppuku
+    if logger is not None:
+        try: print(log_event(logger, 'harikiri', 'shutdown', {}, []))
+        except: pass
+
     call(["/usr/bin/sudo", "/sbin/shutdown", "-h", "now"])
 
 
-def harikiri(root_work, inactivity_secs, check_interval):
+def harikiri(root_work, inactivity_secs, check_interval, logger=None):
     """If no jobs are running and the last job finished more than the 
        threshold, shutdown supervisord gracefully then shutdown the 
        instance.
@@ -138,10 +271,11 @@ def harikiri(root_work, inactivity_secs, check_interval):
     logging.info("root_work_dir=%s" % root_work)
     logging.info("inactivity=%d" % inactivity_secs)
     logging.info("check=%d" % check_interval)
+    logging.info("logger=%s" % logger)
 
     while True:
-        if is_jobless(root_work, inactivity_secs):
-            try: seppuku()
+        if is_jobless(root_work, inactivity_secs, logger):
+            try: seppuku(logger)
             except Exception, e:
                 logging.error("Got exception in seppuku(): %s\n%s" %
                               (str(e), traceback.format_exc()))
@@ -149,13 +283,15 @@ def harikiri(root_work, inactivity_secs, check_interval):
 
 
 if __name__ == "__main__":
-    desc = "HySDS inactivity daemon to perform autoscale group detachment and harikiri"
-    parser = argparse.ArgumentParser(description=desc)
+    parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('root_work_dir',
                         help="root HySDS work directory, e.g. /data/work")
     parser.add_argument('-i', '--inactivity', type=int, default=600,
                         help="inactivity threshold in seconds")
     parser.add_argument('-c', '--check', type=int, default=60,
                          help="check for inactivity every N seconds")
+    parser.add_argument('-l', '--logger', type=str, default=None,
+                         help="enable event logging; specify Mozart REST API," + \
+                              " e.g. https://192.168.0.1/mozart/api/v0.1")
     args = parser.parse_args()
-    harikiri(args.root_work_dir, args.inactivity, args.check)
+    harikiri(args.root_work_dir, args.inactivity, args.check, args.logger)
